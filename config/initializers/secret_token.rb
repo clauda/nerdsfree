89205fd5fdff1f6226d5e8071cf734831939d3ba
Claudia Farias <krawdyah@gmail.com>
2010-11-19 13:22:09 -0300

    rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nerdsfree::Application.config.secret_token = '0c006b4b3d2efffc4771850f4c790678616835a1948b955c317ef907733c44452c41a7254f1c3ae1a037a8c08927fd9d2a82bc496a80241ece785f43e3e13784'
