970450f2fe33299db461cf32aaed329418ca5c9b
Claudia Farias <krawdyah@gmail.com>
2010-11-25 10:46:13 -0300

    rename app at heroku

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -3,15 +3,15 @@ class ConfirmationMailer < ActionMailer::Base
   
   def confirm_mail(nerd)
     @user = nerd
-    @confirm  = "http://nerdsfree.heroku.com/confirm/#{nerd.confirmation.token}"
-    @remove  = "http://nerdsfree.heroku.com/remove/#{nerd.confirmation.token}"
-    mail(:to => nerd.email, :subject => "Nerds for Free")
+    @confirm  = "http://nerdshall.heroku.com/confirm/#{nerd.confirmation.token}"
+    @remove  = "http://nerdshall.heroku.com/remove/#{nerd.confirmation.token}"
+    mail(:to => nerd.email, :subject => "Nerds Hall")
   end
   
   def remove_mail(nerd)
     @user = nerd
-    @url  = "http://nerdsfree.heroku.com/remove/#{nerd.confirmation.token}"
-    mail(:to => nerd.email, :subject => "Nerds for Free")
+    @url  = "http://nerdshall.heroku.com/remove/#{nerd.confirmation.token}"
+    mail(:to => nerd.email, :subject => "Nerds Hall")
   end
   
 end
